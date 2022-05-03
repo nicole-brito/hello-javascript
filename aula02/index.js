e526c11a1122c54ec3d3a0f11d2ad6f02b3c0c5c
Nicole Brito <80689302+nicole-brito@users.noreply.github.com>
2022-05-03 16:53:48 -0300

    Create index.js

---
+++ aula02/index.js
@@ -0,0 +1,7 @@
+//Dá pra colocar js aqui ou no head, mas é melhor no body pq no head ele demora mais pra correga, o melhor é por no final da página
+console.log("Formas de inserir js")
+console.log("Esse trecho será exibido no console do navegador")
+
+alert('Olá mundo!')
+alert('Meu nome é Nicole :)')
+// Não existe essa opção alert em node.js, que funciona no prompt de comando, são dois ambientes diferentes. o browser usa o DOM, o back mexe com outros bgl
