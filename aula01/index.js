6b36d2f9eac0f2e8f4594c5ee8b70aa3f06ebc72
Nicole Brito <80689302+nicole-brito@users.noreply.github.com>
2022-05-03 15:37:08 -0300

    Create index.js

---
+++ aula01/index.js
@@ -0,0 +1,13 @@
+// Formas de inserir um texto 
+console.log('Hello, World'); //String
+console.log("Hello, World"); //String
+console.log(`Hello, World`); //String
+// Serve pra usar aspas dentro do texto
+
+//JS é de tipagem dinâmica
+// Exibindo um  número não se usa aspas, para números quebrados (números de pontos flutuantes) usamos . mas não tem separação, tudo é number
+console.log(123, 15.85, "Nicole Brito");
+
+// Essa é uma forma de comentar (comentário de linha)
+/* Comentário de bloco */ 
+
