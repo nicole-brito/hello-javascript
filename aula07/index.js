78c47b72643d0a719b6e332f94c8e54d56ae0651
Nicole Brito <80689302+nicole-brito@users.noreply.github.com>
2022-05-06 19:07:47 -0300

    Create index.js

---
+++ aula07/index.js
@@ -0,0 +1,28 @@
+/* Aritméticos + - * /
+* + = Adição e Concatenação
+*/ 
+
+const num1 = 5
+const num2 = 5
+console.log(num1 + num2) // 10
+
+const num3 = "5"
+const num4 = "5"
+console.log(num3 + num4) // 55
+
+// ** = Potenciação 
+// % = Resto da Divisão
+
+
+// Operador de incremento ++
+let contador = 1;
+contador++; // 2 // Pré incremento
+contador++; // 3
+contador++; // 4
+++contador; // 5 Nesse caso primeiro ele faz a conta e depois executa a ação
+console.log(contador++) // Aqui ele incrementou também, mas não apareceu pq ele printou antes 
+
+// Decremento --
+let contador1 = 10;
+contador--; // 9 // Pré incremento
+--contador; // 8 // Pós incremento
