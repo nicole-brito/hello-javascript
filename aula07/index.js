3cdd71f2b43273ebb89129d83a416fbdd3e2a6c7
Nicole Brito <nicolem.britto@gmail.com>
2022-05-10 17:54:19 -0300

    Update index.js

--- aula07/index.js
+++ aula07/index.js
@@ -26,3 +26,37 @@ console.log(contador++) // Aqui ele incrementou também, mas não apareceu pq el
 let contador1 = 10;
 contador--; // 9 // Pré incremento
 --contador; // 8 // Pós incremento
+console.log(contador++); // NÃO É UMA BOA PRÁTICA
+
+const passo = 2;
+let contador2 = 0;
+
+contador2 = contador2 + passo;
+console.log(contador2) // Assim pula de dois em dois
+contador2 = contador2 + passo;
+console.log(contador2)
+contador2 = contador2 + passo;
+console.log(contador2)
+
+contador2 += passo; // Assim é mais facil de fazer o bgl de cima
+console.log(contador2)
+
+contador *= 10;
+contador *= 10;
+contador *= 10;
+console.log(contador)
+
+const n1 = 10;
+const n2 = "Abacate";
+console.log(n1 * n2); // Retorna NaN = Resultado inesperado (Not a Number)
+
+// O js sempre tenta corrigir o erro, se fosse um numero dentro da string, ele tentaria corrigir. 
+
+// FORMAS DE CONVERTER STRNG PRA NUMBER
+
+const n3 = 10;
+const n4 = parseInt("5.2") // ele só vê o Inteiro, remove o float  
+const n5 = parseFloat(2.3)
+const n6 = Number(2.8) // Aqui ele se vira
+console.log(n3 + n4 + n5 + n6)
+
