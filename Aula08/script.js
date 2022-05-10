703e0a820ce3ff9d87a490842a06ad3c9c17b0d6
Nicole Brito <nicolem.britto@gmail.com>
2022-05-10 17:54:12 -0300

    Aula 8

---
+++ Aula08/script.js
@@ -0,0 +1,10 @@
+// Métodos
+
+alert('Olá Mundo!') // É um atalho para window.alert
+
+confirm("Deseja realmente apagar?") // sim ou não, boolean 
+
+prompt("Digite o seu nome")
+
+const confirma = confirm("Esse é seu nome mesmo? -.-") // O que for retornado aqui vai para a variavel confirma
+
