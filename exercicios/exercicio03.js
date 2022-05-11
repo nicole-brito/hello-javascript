f4c9c90bd30b21efbf14188deb4c60d2179dbfd8
Nicole Brito <nicolem.britto@gmail.com>
2022-05-11 15:46:29 -0300

    exercício 3

---
+++ exercicios/exercicio03.js
@@ -0,0 +1,9 @@
+let n1 = prompt('Digite um número');
+let n2 = prompt('Digite outro número');
+
+n1 = Number(n1);
+n2 = Number(n2);
+
+const total = n1 + n2;
+
+alert(`O resultado da sua soma é ${total}`)
